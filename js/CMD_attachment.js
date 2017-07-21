90c8b8a04c35dc65c8e2703bd363958ac181e14b
David Lin <davidlin98@hotmail.com>
2017-07-20 17:05:54 -0700

    Update CMD_attachment.js
    Solved jquery loop problem

--- js/CMD_attachment.js
+++ js/CMD_attachment.js
@@ -4,15 +4,10 @@
 
 		var items = ['syllabi','resources'];
 
-
-
-		for(var i = 0; i < items.length ; i++ ) {
-			var name = items[i];
-			// Check to see if the 'Delete File' link exists on the page...
-			if($('a#'+ name +'_file_delete').length === 1) {
+		$.each(items, function(key, val) {
 
 				// Since the link exists, we need to handle the case when the user clicks on it...
-				$('#'+ name +'_file_delete').click(function(e) {
+				$('#'+ val +'_file_delete').click(function(e) {
 
 					// We don't want the link to remove us from the current page
 					// so we're going to stop it's normal behavior.
@@ -20,17 +15,14 @@
 
 					// Find the text input element that stores the path to the file
 					// and clear it's value.
-					$('#'+ name +'_file_url').val('');
+					$('#'+ val +'_file_url').val('');
 
 					// Hide this link so users can't click on it multiple times
 					$(this).hide();
 
 				});
 
-			} // end if
-		}
-
-
+		});
 
   });
 
